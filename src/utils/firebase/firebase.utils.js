62358660eac0e6e6dfb0c61d6e6177314a57c158
shimy8617 <cristinashim@yahoo.com.ar>
2022-08-30 11:41:42 -0300

    me

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -33,7 +33,29 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     
     console.log(userDocRef);
 
-    const userSnapShot = await getDoc(userDocRef);
-    console.log(userSnapShot);
-    console.log(userSnapShot.exists);
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    //if user data does no exists
+    //create / set the document with the data from userAuth in my collection
+    
+    if(!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message)
+      }
+    }
+
+    //if user data exists
+    
+    return userDocRef;
 }
