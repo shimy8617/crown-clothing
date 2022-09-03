a1cace67c71d0acfec20467fe613f74012bbcfc7
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-03 11:33:49 -0300

    me

--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -1,18 +1,12 @@
-import { signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
-
 import SignUpForm from '../../components/sign-up-form/sign-up-form';
 import SignInForm from '../../components/sign-in-form/sign-in-form';
 
+import './authentication.styles.scss';
+
 const Authentication = () => {
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    };
 
     return(
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <div className='authentication-container'>
             <SignInForm />
             <SignUpForm />
             </div>
