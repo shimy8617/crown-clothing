84849e5bd5fce6bfa4ddb61865468f23a4d0d269
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-07 15:26:18 -0300

    me

--- src/components/sign-up-form/sign-up-form.jsx
+++ src/components/sign-up-form/sign-up-form.jsx
@@ -40,9 +40,6 @@ const SignUpForm = () => {
                 password
                 );
 
-                setCurrentUser();
-
-                await createUserDocumentFromAuth(user, { displayName });
                 resetFormFields();
         } catch(error) {
             if(error.code === 'auth/email-already-in-use') {
