6075b60bce93b7bbd835e38cf3651df4c30ac115
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-12 16:08:46 -0300

    checkout item part 3

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -34,12 +34,15 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
   removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
 });
 
@@ -64,11 +67,16 @@ export const CartProvider = ({ children }) => {
     setCartItems(removeCartItem(cartItems, cartItemToRemove));
   }
 
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  }
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
     removeItemFromCart,
+    clearItemFromCart,
     cartItems,
     cartCount,
   };
