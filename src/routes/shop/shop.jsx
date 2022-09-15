656c588670158e393128b369c1c40ab1f47d83ea
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-15 13:37:49 -0300

    nested routes

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,22 +1,14 @@
-import { Fragment, useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview';
-
-import ProductCard from '../../components/product-card/product-card';
+import CategoriesPreview from '../categories-preview/categories-preview';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-
     return (
-        <div className='shop-container'>
-            {Object.keys(categoriesMap).map((title) => {
-                const products = categoriesMap[title];
-                return <CategoryPreview key={title} title={title} product={products} />
-            })}
-        </div>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+        </Routes>
     );
 };
 
