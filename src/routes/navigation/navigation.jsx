ffe4aa0d02df83d1c4fab0b37e0ac4fcc02f6a7c
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-26 13:32:23 -0300

    me

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,10 +1,10 @@
 import { Fragment, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
@@ -13,7 +13,7 @@ import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { NavigationContainer, NavLinksContainer, NavLink, LogoContainer } from './navigation.styles.jsx';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector((state) => state.user.currentUser);
   const { isCartOpen } = useContext(CartContext);
 
     return (
