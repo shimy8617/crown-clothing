fe22373f32414c83c2bc2203100e30b5b0d56a5f
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-27 16:28:42 -0300

    reselect library

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,26 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired');
-  return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => {
+  console.log('selector fired 1');
+  return state.categories;
+}
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    console.log('selector fired 2');
+    return categoriesSlice.categories
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('selector fired 3')
+  categories.reduce((acc, category) => {
   const { title, items } = category;
   acc[title.toLowerCase()] = items;
   return acc;
-}, {});
-};
+}, {})
+}
+);
