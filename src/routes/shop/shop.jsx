5347db6a38ca1cbe02f9f9917787c92190087a1b
shimy8617 <cristinashim@yahoo.com.ar>
2022-09-27 11:58:57 -0300

    buiseness logic selector

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -5,15 +5,15 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview';
 import Category from '../category/category';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action'; 
+import { setCategories } from '../../store/categories/category.action'; 
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments('categories');
-                dispatch(setCategoriesMap(categoryMap));
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            dispatch(setCategories(categoriesArray)); 
 ;        }
         getCategoriesMap();
     }, []);
